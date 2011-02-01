4da853f6d5351fb536881d013353db70b76d450e
Raymond Hill <rhill@raymondhill.net>
2011-02-01 11:00:13 -0500

    Added support to select a suggestion using keyboard's arrow keys/enter

--- cablegate-search.js
+++ cablegate-search.js
@@ -12,6 +12,7 @@ if (!CablegateObject) {
 	var co = CablegateObject;
 	co.suggestionKeyupTimer = null;
 	co.suggestionCache = {};
+	co.suggestionSelected = -1;
 
 	var suggestionsRequestHandler = function(response) {
 		if (response.suggestions && response.suggestions.length) {
@@ -20,9 +21,9 @@ if (!CablegateObject) {
 		syncSuggestions();
 		};
 
-	// Returns the content of the input field as a
-	// list of words/sequences of spaces, and which
-	// word is touching the caret.
+	// Returns the content of the input field as an
+	// alternate list of word and non-word segments,
+	// and which segment is in contact with the caret.
 	var getInputFieldDetails = function() {
 		var q = $('q');
 
@@ -35,18 +36,20 @@ if (!CablegateObject) {
 				};
 			}
 
-		// Find which word is at caret position
+		// Find which word is at caret position. If the caret
+		// touches both a word and non-word segment simultaneously,
+		// the word has priority
 		var caret = q.getCaretPosition(),
 			i, n = matches.length,
 			beg, end = 0,
-			word, startwith;
+			word;
 
 		for (i=0; i<n; i++) {
 			beg = end;
 			if (caret < beg) {break;}
 			word = matches[i];
 			end = beg + word.length;
-			if (caret < end || (/[a-zA-Z0-9]/.test(word) && caret == end)) {break;}
+			if (caret < end || (/[a-zA-Z0-9]/.test(word) && caret === end)) {break;}
 			}
 		return {
 			which: i < n ? i : -1,
@@ -60,9 +63,9 @@ if (!CablegateObject) {
 	// Can be asynchronous if the list of suggestions is
 	// not readily available.
 	var getSuggestions = function() {
-		var startwith;
-		var details = getInputFieldDetails();
-		var which = details.which,
+		var startwith,
+			details = getInputFieldDetails(),
+			which = details.which,
 			words = details.words;
 		if (which >= 0 && /[a-zA-Z0-9]/.test(words[which])) {
 			startwith = words[which].replace(/^=/,'').toLowerCase();
@@ -79,25 +82,25 @@ if (!CablegateObject) {
 		// Nope, will have to get it from server
 		var options = {
 			url:'cablegate-do.php',
-			onSuccess: suggestionsRequestHandler,
+			onSuccess: suggestionsRequestHandler
 			};
 		var parms = {
 			command: 'get_keyword_suggestions',
 			startwith: startwith,
-			limit: 10,
+			limit: 10
 			};
 		var dummy = new Request.JSON(options).get(parms);
 
 		return [];
 		};
 
-	var suggestionsClickHandler = function() {
+	var useSuggestion = function(suggestion) {
 		var details = getInputFieldDetails();
 		var which = details.which,
 			words = details.words;
 		if (which >= 0 && /[a-zA-Z0-9]/.test(words[which])) {
 			var q = $('q');
-			words[which] = this.innerHTML.replace(/\s+\S+$/,'');
+			words[which] = suggestion.innerHTML.replace(/\s+\S+$/,'');
 			if (which === words.length-1) {
 				words.push(' ');
 				}
@@ -106,34 +109,67 @@ if (!CablegateObject) {
 			q.setCaretPosition(details.sOffset + words[which].length + words[which+1].length);
 			syncSuggestions(); // caret moved, resync
 			}
+		};
 
+	var selectSuggestion = function(index) {
+		if (index === co.suggestionSelected) {return;}
+		var ss = $('search-suggestions');
+		ss.getElements('.selected').each(function(e){
+			e.removeClass('selected');
+			});
+		var ee = ss.getElements('div');
+		if (index < -1) {
+			index = ee.length-1;
+			}
+		if (ee[index] !== undefined) {
+			ee[index].addClass('selected');
+			co.suggestionSelected = index;
+			}
+		else {
+			co.suggestionSelected = -1;
+			}
+		};
+
+	var suggestionMouseenterHandler = function() {
+		selectSuggestion($$('#search-suggestions > div').indexOf(this));
+		};
+
+	var suggestionMouseleaveHandler = function() {
+		if (co.suggestionSelected === $$('#search-suggestions > div').indexOf(this)) {
+			selectSuggestion(-1);
+			}
+		};
+
+	var suggestionClickHandler = function() {
+		useSuggestion(this);
 		// cancel event, to avoid input field from losing focus
 		return false;
 		};
 
 	var syncSuggestions = function(show) {
 		// Suggestions are shown if:
+		//   'show' is not false
 		//   The input field has focus
 		//   There is at least one suggestion matching word at caret position
-		//   'show' is not false
-		var q = $('q');
-		var suggestions = [];
-
-		// Does the input have focus?
-		if (q.retrieve('hasFocus',false)) {
-			suggestions = getSuggestions();
-			}
+		var q = $('q'),
+			suggestions = q.retrieve('hasFocus',false) ? getSuggestions() : [];
 
 		// Something to show?
 		var ss = $('search-suggestions');
 		ss.empty();
 		if (suggestions.length) {
-			var prefix = $('q').value.search(/^=/) >= 0 ? '=' : '';
-			for (var i=0; i < suggestions.length; i++) {
-				var suggestion = suggestions[i];
+			var prefix = $('q').value.search(/^=/) >= 0 ? '=' : '',
+				n = suggestions.length,
+				html;
+			while (n-- > 0) {
+				html = prefix + suggestions[n];
 				ss.grab(new Element('div', {
-					html: prefix + suggestion,
-					events: {mousedown: suggestionsClickHandler},
+					html: html,
+					events: {
+						mouseenter: suggestionMouseenterHandler,
+						mouseleave: suggestionMouseleaveHandler,
+						mousedown: suggestionClickHandler
+						}
 					}));
 				}
 			}
@@ -147,14 +183,26 @@ if (!CablegateObject) {
 		syncSuggestions();
 		};
 
-	var keyupHandler = function() {
+	var changeHandler = function(event) {
+		// This timer is to avoid firing up requests
+		// for every single key up event
 		if (co.suggestionKeyupTimer) {
 			window.clearTimeout(co.suggestionKeyupTimer);
 			}
-		// this timer is to avoid firing up
-		// requests for every single key up
-		// event
 		co.suggestionKeyupTimer = window.setTimeout(keyupTimerHandler,250);
+		};
+
+	var keyupHandler = function(event) {
+		// Handle arrow keys
+		if (event.key === 'up') {
+			selectSuggestion(co.suggestionSelected-1);
+			return false; // event consumed
+			}
+		if (event.key === 'down') {
+			selectSuggestion(co.suggestionSelected+1);
+			return false; // event consumed
+			}
+		changeHandler();
 		};
 
 	var clickHandler = function() {
@@ -171,16 +219,26 @@ if (!CablegateObject) {
 		syncSuggestions();
 		};
 
+	var submitHandler = function(event) {
+		var ee = $$('#search-suggestions > div');
+		if (ee[co.suggestionSelected] !== undefined) {
+			useSuggestion(ee[co.suggestionSelected]);
+			selectSuggestion(-1);
+			return false;
+			}
+		};
+
 	var init = function(){
 		var q = $('q');
-		q.setAttribute("autocomplete","off");
+		q.setAttribute('autocomplete','off');
 		q.addEvents({
 			click: clickHandler,
 			keyup: keyupHandler,
-			change: keyupHandler,
+			change: changeHandler,
 			focus: focusHandler,
-			blur: blurHandler,
+			blur: blurHandler
 			});
+		q.form.addEvent('submit', submitHandler);
 		};
 
 	window.addEvent('domready', function(){init();});
