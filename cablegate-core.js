191423355f588dca7fedc1619707788fb6cb967c
Raymond Hill <rhill@raymondhill.net>
2011-03-05 21:17:04 -0500

    Committing all cumulated changes since a while now

--- cablegate-core.js
+++ cablegate-core.js
@@ -9,14 +9,6 @@
 			e.href='mailto:'+text+'?subject=Cablegate: full-text search';
 			e.set('text',text);
 			}
-		// private cart tooltip
-		$$('a[href="/cart.php"]').each(function(e){
-			var tipElem = $('cart-tips');
-			tipElem.setStyle('display','none');
-			var dummy = new Tips(e,{fixed:true,offset:{x:20,y:24}});
-			e.store('tip:title','');
-			e.store('tip:text',tipElem.get('html'));
-			});
 		};
 
 	window.addEvent('domready',init);
