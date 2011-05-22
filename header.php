e1f23ff2905ff97d7b3ad6af6d6028633ccb9dee
Raymond Hill <rhill@raymondhill.net>
2011-05-22 09:53:02 -0400

    Added the ability to have a JPG image in the tip

--- header.php
+++ header.php
@@ -9,10 +9,9 @@ $navi_entries = array(
 	array('Private cart', '/cart.php', 'Here you can see and manage the content of your private cart.'),
 	array('Extras', '/extras.php', 'Unsorted stuff as of now.'),
 	array('WL Central', 'http://wlcentral.org/', 'WikiLeaks news, analysis and action (external site).'),
-//	array('Julian Assange speech in Melbourne', 'http://www.youtube.com/watch?v=zL6433U87HU',''),
-//	array('Four Corners: &ldquo;The Forgotten Man&rdquo;','http://www.abc.net.au/4corners/special_eds/20110214/leaks/default.htm',''),
-//	array('&ldquo;Bradley Manning could face death: for what?&rdquo;', 'http://www.salon.com/news/opinion/glenn_greenwald/2011/03/03/manning/','Manning: &ldquo;well, it was forwarded to [WikiLeaks] ... hopefully worldwide discussion, debates, and reforms ... i want people to see the truth ... regardless of who they are ... because without information, you cannot make informed decisions as a public.&rdquo;')
-	array('SMH: &ldquo;Cutting Assange loose demeans our nation&rdquo;', 'http://www.smh.com.au/opinion/politics/cutting-assange-loose-demeans-our-nation-20110311-1br15.html','... Contrast the deafening Australian silence in response to Assange&rsquo;s pleas for help, with the pressure and determination being exercised by the US in support of an American citizen &ndash; a CIA operative who killed two Pakistanis in front of witnesses, he claims in self defence. ...<br><br>In another serious misjudgment, though, Gillard has failed to see Assange is widely admired for his courage and determination - WikiLeaks has contributed to enhancing democracy globally. ...')
+	array('The magic of WikiLeaks: telling it like it is', 'http://www.theage.com.au/national/the-magic-of-wikileaks-telling-it-like-it-is-20110503-1e6ob.html','The battle over WikiLeaks is all about information - who owns it, who controls it, who needs it - and about one man\'s idea to set it free.'),
+	array('Andrew Fowler on Assange', 'http://www.abc.net.au/local/stories/2011/04/07/3185014.htm','Investigative journalist Andrew Fowler has written how an Aussie computer hacker became the &ldquo;Most Dangerous Man in the World&rdquo;')
+//	array('1. Article 19 of UDHR', 'http://www.un.org/en/documents/udhr/index.shtml#a19','Everyone has the right to freedom of opinion and expression; this right includes <b>freedom to hold opinions without interference and to seek, receive and impart information and ideas</b> through any media and regardless of frontiers.'),
 	);
 foreach ( $navi_entries as $entry ) {
 	list($prompt, $url, $title) = $entry;
@@ -23,19 +22,33 @@ foreach ( $navi_entries as $entry ) {
 		printf('<span>%s</span>', $prompt);
 		}
 	}
-?></div><a href="http://march12.rsf.org/en/" style="margin:0;padding:0;display:block;position:absolute;right:2px;width:128px;height:128px;background:transparent url('13.png') no-repeat right top;z-index:0;cursor:pointer" title="World day against cyber-censorship"></a>
+?></div>
 <script type="text/javascript">
 <!--
 (function(){
 	window.addEvent('domready',function(){
 		$$('#navi a').each(function(e){
+			var tipContent = e.getProperty('title');
+			var className = 'tiptext';
+			var matches = tipContent.match(/[^ ]+\.jpg/g);
+			if (matches && matches.length) {
+				tipContent = '';
+				for (var i=0; i<matches.length; i++) {
+					if (i) {
+						tipContent += '<br>';
+						}
+					tipContent += '<img src="' + matches[i] + '">';
+					}
+				className = 'tipimage';
+				}
 			var dummy = new Tips(e,{
 				showDelay:250,
+				className: className,
 				title:'',
-				text:'title',
 				offset:{y:24},
 				fixed:true
 				});
+			e.store('tip:text', tipContent);
 			});
 		});
 }());
