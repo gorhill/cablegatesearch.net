7efb6e555c0ae0aab5c164549180cb072d59fe84
Raymond Hill <rhill@raymondhill.net>
2011-07-13 22:04:59 -0400

    Bringing all sources up to date with current online state

--- header.php
+++ header.php
@@ -4,23 +4,27 @@
 <div id="navi"><?php
 $navi_entries = array(
 	array('Full-text search', '/search.php', 'Perform a full-text search of all released cables so far.'),
-	array('Browse tags', '/browse.php', 'Browse the tags: Countries, origins, subjects, programs, organizations.'),
-	array('Publishing history', '/history.php', 'What was published, modified, removed, and when.'),
 	array('Private cart', '/cart.php', 'Here you can see and manage the content of your private cart.'),
-	array('Extras', '/extras.php', 'Unsorted stuff as of now.'),
+	array('Browse tags', '/browse.php', 'Browse the tags: Countries, origins, subjects, programs, organizations.'),
+	array('Overview', '/overview.php', 'Overview of the published vs. unpublished status of all cables.'),
+	array('Publishing history', '/history.php', 'What was published, modified, removed, and when.'),
+	array('Media', '/media.php', 'Media coverage of the Cablegate&rsquo;s cables'),
 	array('WL Central', 'http://wlcentral.org/', 'WikiLeaks news, analysis and action (external site).'),
 	array('The magic of WikiLeaks: telling it like it is', 'http://www.theage.com.au/national/the-magic-of-wikileaks-telling-it-like-it-is-20110503-1e6ob.html','The battle over WikiLeaks is all about information - who owns it, who controls it, who needs it - and about one man\'s idea to set it free.'),
-	array('Andrew Fowler on Assange', 'http://www.abc.net.au/local/stories/2011/04/07/3185014.htm','Investigative journalist Andrew Fowler has written how an Aussie computer hacker became the &ldquo;Most Dangerous Man in the World&rdquo;')
-//	array('1. Article 19 of UDHR', 'http://www.un.org/en/documents/udhr/index.shtml#a19','Everyone has the right to freedom of opinion and expression; this right includes <b>freedom to hold opinions without interference and to seek, receive and impart information and ideas</b> through any media and regardless of frontiers.'),
+	array('PFC Bradley Manning, Conscience &amp; Agency', 'http://www.michaelmoore.com/words/mike-friends-blog/pfc-bradley-manning','&ldquo;The contents of the WikiLeaks revelations have pulled back the curtain on the degradation of our democratic system. It has become completely normal for decision-makers to promulgate foreign policies, diplomatic strategies, and military operating procedures that are hostile to the democratic ideals our country was founded upon. The incident I was part of&ndash;shown in the Collateral Murder video&ndash;becomes even more horrific when we grasp that it was not exceptional.&rdquo;')
+//	array('Andrew Fowler on Assange', 'http://www.abc.net.au/local/stories/2011/04/07/3185014.htm','Investigative journalist Andrew Fowler has written how an Aussie computer hacker became the &ldquo;Most Dangerous Man in the World&rdquo;')
+//	array('Article 19 of UDHR', 'http://www.un.org/en/documents/udhr/index.shtml#a19','Everyone has the right to freedom of opinion and expression; this right includes <b>freedom to hold opinions without interference and to seek, receive and impart information and ideas</b> through any media and regardless of frontiers.'),
 	);
+$separator = '';
 foreach ( $navi_entries as $entry ) {
 	list($prompt, $url, $title) = $entry;
 	if ( strcmp($_SERVER['REQUEST_URI'], $url) ) {
-		printf('<a href="%s" title="%s">%s</a> ', $url, $title, $prompt);
+		printf('%s<a href="%s" title="%s">%s</a>', $separator, $url, $title, $prompt);
 		}
 	else {
-		printf('<span>%s</span>', $prompt);
+		printf('%s<span>%s</span>', $separator, $prompt);
 		}
+	$separator = ' &bull; ';
 	}
 ?></div>
 <script type="text/javascript">
