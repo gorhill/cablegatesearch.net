e3b43a721dbba468224346ebadcc4d8b5898f3ea
Raymond Hill <rhill@raymondhill.net>
2011-08-31 08:23:20 -0400

    Bringing project up to date with live version at cablegatesearch.net

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div style="margin:1em 0 1em 0;border-top:1px solid #aaa;height:1px"></div>
 <p style="color:gray;font-size:smaller"><b>Contact (suggestions, flames, etc.):</b> <a href="#mangled-email" id="mangled-email">[email not available without Javascript]</a> (<a href="release-notes.php">Release notes</a>)<br> 
 <b>Credits:</b> <a style="font-style:italic" href="http://mootools.net/">Mootools</a> for Javascript framework; <a style="font-style:italic" href="http://openiconlibrary.sourceforge.net/">Open Icon Library</a> for the some of the icons used on this page; <a href="http://cabletags.wordpress.com/2010/12/12/wikileaks-cable-tags-explained/">Cabletags</a>, for its definitions of cable tags.<br>
-<b>Notes:</b> The author of this site is not affiliated with <a href="http://213.251.145.96/">Wikileaks</a>. This work though (<a href="https://github.com/gorhill/cablegatesearch.net">available on Github</a>), is a way to express my support for Wikileaks, following <a href="http://www.raymondhill.net/blog/?p=209">Canadian Tom Flanagan&rsquo;s suggestion that Julian Assange should be assassinated</a>.<br>
+<b>Notes:</b> This work (<a href="https://github.com/gorhill/cablegatesearch.net">available on Github</a>), is a way to express my support for Wikileaks, following <a href="http://www.raymondhill.net/blog/?p=209">Canadian Tom Flanagan&rsquo;s suggestion that Julian Assange should be assassinated</a>.<br>
 <b>Reminder:</b> <a href="http://www.alternet.org/module/printversion/149369">&ldquo;Eight Smears and Misconceptions About WikiLeaks Spread By the Media&rdquo;</a><br>
 </p>
 <script type="text/javascript">
