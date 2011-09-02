0c26ba13c85ccff17de0c346c5cd8f5bfadac5e8
Raymond Hill <rhill@raymondhill.net>
2011-09-02 12:51:27 -0400

    Updated with torrent for latest SQL dump, which contains ALL diplomatic cables

--- cablegatesearch-create.sql
+++ cablegatesearch-create.sql
@@ -269,4 +269,4 @@ CREATE TABLE `cablegate_urlassoc` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-08-31 12:30:41
+-- Dump completed on 2011-09-02 12:41:55
