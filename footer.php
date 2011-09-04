4fe6f026b2363c10ea62336191b900c44497b327
Raymond Hill <rhill@raymondhill.net>
2011-09-03 23:39:34 -0400

    Trivial visual stuff

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div style="margin:1em 0 1em 0;border-top:1px solid #aaa;height:1px"></div>
+<div style="margin:1em 0 1em 0;border:1px solid #aaa;border-style:solid none;height:1px"></div>
 <p style="color:gray;font-size:smaller"><b>Contact (suggestions, flames, etc.):</b> <a href="#mangled-email" id="mangled-email">[email not available without Javascript]</a> (<a href="release-notes.php">Release notes</a>)<br> 
 <b>Credits:</b> <a style="font-style:italic" href="http://mootools.net/">Mootools</a> for Javascript framework; <a style="font-style:italic" href="http://openiconlibrary.sourceforge.net/">Open Icon Library</a> for the some of the icons used on this page; <a href="http://cabletags.wordpress.com/2010/12/12/wikileaks-cable-tags-explained/">Cabletags</a>, for its definitions of cable tags.<br>
 <b>Notes:</b> This work (<a href="https://github.com/gorhill/cablegatesearch.net">available on Github</a>), is a way to express my support for Wikileaks, following <a href="http://www.raymondhill.net/blog/?p=209">Canadian Tom Flanagan&rsquo;s suggestion that Julian Assange should be assassinated</a>.<br>
